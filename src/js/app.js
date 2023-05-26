845bfc115e390fb63a4fc6d244564f6e3ed5b3d7
yoshicon <shreyaskgantayet908@gmail.com>
2023-05-26 17:46:10 +0530

    added num, ac, ce buttons visual functionality

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,25 @@
 "use strict";
-// assigning number to each number
-// function 
-// function numBut(num: number) {
-//     return function();
-// }
+let activeTextBox = document.getElementById("activeText");
+let historyTextBox = document.getElementById("historyText");
+document.querySelectorAll(".numBut").forEach((button) => {
+    button.addEventListener("click", (e) => {
+        let numText = button.getAttribute("num");
+        // @ts-ignore
+        activeTextBox.innerHTML += numText;
+    });
+});
+// Addinf the AC and CE button functionality
+// @ts-ignore
+document.querySelector("#acBut").addEventListener("click", (e) => {
+    console.log("AC button clicked");
+    // @ts-ignore
+    activeTextBox.textContent = "";
+    // @ts-ignore
+    historyTextBox.textContent = "";
+});
+// @ts-ignore
+document.getElementById("ceBut").addEventListener("click", (e) => {
+    // @ts-ignore
+    activeTextBox.textContent = "";
+});
 //# sourceMappingURL=app.js.map
--- src/ts/app.ts
+++ src/ts/app.ts
@@ -1,8 +1,31 @@
-// assigning number to each number
+let activeTextBox = document.getElementById("activeText");
+let historyTextBox = document.getElementById("historyText");
 
-// function 
+document.querySelectorAll(".numBut").forEach((button) => {
+    button.addEventListener("click", (e) => {
+        let numText = button.getAttribute("num");
 
-// function numBut(num: number) {
-//     return function();
-// }
+        // @ts-ignore
+        activeTextBox.innerHTML += numText;
+    });
+}
+)
 
+// Addinf the AC and CE button functionality
+// @ts-ignore
+
+document.querySelector("#acBut").addEventListener("click", (e) => {
+    console.log("AC button clicked");
+    // @ts-ignore
+    activeTextBox.textContent = "";
+    // @ts-ignore
+    historyTextBox.textContent = "";
+    
+}
+)
+// @ts-ignore
+document.getElementById("ceBut").addEventListener("click", (e) => {
+    // @ts-ignore
+    activeTextBox.textContent = "";
+}
+)
